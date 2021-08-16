86a30720db806025b9cd6078527e6988c2cddfc1
Tarun Yadav <tarunyadav9761@gmail.com>
2021-08-16 12:36:22 +0530

    gatsby-source-filesystem remove

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,13 +105,13 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
